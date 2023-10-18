99f7855825a3d899721d79719f430d4611c694e3
Hamid Medgjonaj <hmedgjonaj@gmail.com>
2023-10-18 10:50:57 +0200

    - Implemented HtmlParser using HtmlAgilityPack package for parsing the content of the web page

---
+++ Scrapify.Core/Parsers/HtmlParser.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+using Scrapify.Core.Interfaces;
+using Scrapify.Core.Models;
+
+namespace Scrapify.Core.Parsers;
+
+internal sealed class HtmlParser : IParser<WebPage>
+{
+    public WebPage Parse(string url, string content)
+    {
+        var webPage = new WebPage()
+        {
+            Content = content
+        };
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(content);
+
+        foreach(var htmlNode in htmlDocument.DocumentNode.DescendantsAndSelf())
+        {
+            var resourceName = htmlNode.GetAttributeValue("src", null) 
+                ?? htmlNode.GetAttributeValue("href", null);
+
+            if (IsValidResource(resourceName))
+            {
+                var resourceUrl = new Uri(new Uri(url), resourceName).AbsoluteUri;
+                webPage.Resources.Add(new WebResource(resourceName, resourceUrl));
+            }
+        }
+
+        return webPage;
+    }
+
+    private static bool IsValidResource(string? resourceName)
+    {
+        return !string.IsNullOrEmpty(resourceName)
+            && !resourceName.Contains("http://")
+            && !resourceName.Contains("https://");
+    }
+}

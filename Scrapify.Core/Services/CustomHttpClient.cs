90951235625b09d0f21769a9343bff5966c60f52
Hamid Medgjonaj <hmedgjonaj@gmail.com>
2023-10-18 12:51:43 +0200

    - Implemented CustomHttpClient

---
+++ Scrapify.Core/Services/CustomHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using Scrapify.Core.Interfaces;
+
+namespace Scrapify.Core.Services;
+
+public class CustomHttpClient : IHttpClient
+{
+    private readonly HttpClient _httpClient;
+    public CustomHttpClient()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string> GetContent(string url, CancellationToken cancellationToken = default)
+    {
+        var response = await _httpClient.GetAsync(url, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    public async Task<Stream> GetResourceStream(string resourceUrl, CancellationToken cancellationToken = default)
+    {
+        var response = await _httpClient.GetAsync(resourceUrl, cancellationToken);
+        return await response.Content.ReadAsStreamAsync(cancellationToken);
+    }
+}
--- Scrapify.UnitTests/HtmlParserTests.cs
+++ Scrapify.UnitTests/HtmlParserTests.cs
@@ -1,44 +1,43 @@
 using Scrapify.Core.Parsers;
 
-namespace Scrapy.UnitTests
+namespace Scrapify.UnitTests;
+
+public class HtmlParserTests
 {
-    public class HtmlParserTests
-    {
-        private static readonly string Url = "https://www.w3schools.com";
-        private static readonly string HtmlContent = """
+    private static readonly string Url = "https://www.w3schools.com";
+    private static readonly string HtmlContent = """
                 <!DOCTYPE html>
                 <html>
                 <body>
-                    <a href="https://www.w3schools.com">This is a link</a>
+                    <a href="https://www.w3schools.com">This is an external link</a>
                     <img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
                 </body>
                 </html>
                 """;
 
-        [Fact]
-        public void Parse_Should_Return_WebPage_With_HTMLContent()
-        {
-            // Arrange
-            var htmlParser = new HtmlParser();
+    [Fact]
+    public void Parse_Should_Return_WebPage_With_HTMLContent()
+    {
+        // Arrange
+        var htmlParser = new HtmlParser();
 
-            // Act
-            var webPage = htmlParser.Parse(Url, HtmlContent);
+        // Act
+        var webPage = htmlParser.Parse(Url, HtmlContent);
 
-            // Assert
-            Assert.Equal(webPage.Content, HtmlContent);
-        }
+        // Assert
+        Assert.Equal(webPage.Content, HtmlContent);
+    }
 
-        [Fact]
-        public void Parse_Should_Return_Exact_Number_Of_Resources()
-        {
-            // Arrange
-            var htmlParser = new HtmlParser();
+    [Fact]
+    public void Parse_Should_Return_Exact_Number_Of_Resources()
+    {
+        // Arrange
+        var htmlParser = new HtmlParser();
 
-            // Act
-            var webPage = htmlParser.Parse(Url, HtmlContent);
+        // Act
+        var webPage = htmlParser.Parse(Url, HtmlContent);
 
-            // Assert
-            Assert.Equal(1, webPage.GetResources().Count);
-        }
+        // Assert
+        Assert.Equal(1, webPage.GetResources().Count);
     }
 }
